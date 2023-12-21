099b5a3bbe1fa3ac331f55425d78ada0398af713
c1334690 <joao.ribeiro@capgemini.com>
2023-12-21 17:02:57 -0300

    feat(iq05-transition-animated): implementa transição de animação

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,5 +1,10 @@
 import { Pressable, PressableProps, Text} from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import Animated, { 
+  useSharedValue, 
+  useAnimatedStyle, 
+  withTiming, 
+  Easing 
+} from 'react-native-reanimated';
 
 import { THEME } from '../../styles/theme';
 import { styles } from './styles';
@@ -29,11 +34,11 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
   });
 
   function onPressIn(){
-    scale.value = 1.1;
+    scale.value = withTiming(1.1, {easing: Easing.bounce, duration: 700});
   }
 
   function onPressOut(){
-    scale.value = 1;
+    scale.value = withTiming(1, {easing: Easing.bounce, duration: 700});
   }
 
   return (
